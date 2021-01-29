ae8bf3093f396eddd847a0c54eeef344f54a7697
Marcin <marcinskalski1993@gmail.com>
2021-01-29 22:25:59 +0100

    project cleanup and correcting .gitignore

--- src/main/java/application/core/controller/DemoController.java
+++ src/main/java/application/core/controller/DemoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
@@ -23,20 +24,16 @@ public class DemoController {
     }
 
     // == request methods ==
-
-    // http://localhost:8080/todo-app/hello
     @ResponseBody
     @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
 
-    // http://localhost:8080/todo-app/welcome
     @GetMapping("welcome")
-    // prefix + name + suffix
-    //WEB-INF/view/welcome.jsp
-    public String welcome(Model model) {
-        model.addAttribute("helloMessage", demoService.getHelloMessage("Wiedzmin"));
+    public String welcome(@RequestParam String user, @RequestParam int age, Model model) {
+        model.addAttribute("helloMessage", demoService.getHelloMessage(user));
+        model.addAttribute("age", age);
         log.info("Model = {} ", model);
         return "welcome";
     }
--- src/main/webapp/WEB-INF/view/welcome.jsp
+++ src/main/webapp/WEB-INF/view/welcome.jsp
@@ -5,6 +5,7 @@
 </head>
 <body>
     <h1>${helloMessage}</h1>
+    <h2>Age is${age}</h2>
     <h2>${welcomeMessage}</h2>
 </body>
 </html>
