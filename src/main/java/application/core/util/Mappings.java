f797587cfee0be4589ff5092467e4610d19e05ce
Marcin <marcinskalski1993@gmail.com>
2021-02-21 19:09:38 +0100

    added home page view with hiperlinks

--- src/main/java/application/core/util/Mappings.java
+++ src/main/java/application/core/util/Mappings.java
@@ -5,6 +5,7 @@ public final class Mappings {
     // == constans ==
     public static final String ITEMS = "items";
     public static final String ADD_ITEM = "add_item";
+    public static final String DELETE_ITEM = "deleteItem";
 
     // == constructors ==
     private Mappings(){}
